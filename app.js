940e57dca8b0b1913d357fa359ddc52ee3bc124b
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2020-12-17 19:29:51 +0530

    add core

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 app.use(
   cors({
-      origin: [ 'https://mvpnote-frontend.herokuapp.com','https://mvpnote-frontend.herokuapp.com/','http://mvpnote-frontend.herokuapp.com'],
+      origin: ['https://mvpnotes-backend.herokuapp.com/adminLogin', 'https://mvpnote-frontend.herokuapp.com'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       'Access-Control-Allow-Header':
       'Origin, X-Requested-With, Content-Type, Accept',
